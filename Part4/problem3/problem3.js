729db93b95ec90f412685be5f2875331f4f858e4
unknown <wahyuramadhan0549@gmail.com>
2022-06-30 08:59:06 +0700

    problem3-part4 was cleared

--- Part4/problem3/problem3.js
+++ Part4/problem3/problem3.js
@@ -1,5 +1,17 @@
 function joinArrayRemoveDuplicate(arrayA, arrayB) {
   // Your Code Here
+  let result = [...arrayA,...arrayB];
+  return result;
 }
 
-module.exports = joinArrayRemoveDuplicate;
+// Test cases
+console.log(joinArrayRemoveDuplicate(["apel", "anggur"], ["lemon", "leci", "nanas"]))
+// ["apel", "anggur", "lemon", "leci", "nanas"]
+ 
+console.log(joinArrayRemoveDuplicate(["samsung", "apple"], ["apple", "sony", "xiaomi"]))
+// ["samsung", "apple", "sony", "xiaomi"]
+ 
+console.log(joinArrayRemoveDuplicate(["football", "basketball"], ["basketball", "football"]))
+// [“football”, “basketball”]
+
+// module.exports = joinArrayRemoveDuplicate;
